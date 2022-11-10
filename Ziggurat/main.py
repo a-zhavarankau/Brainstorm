33429e7672a01c4fcf4df2e56745e0f4e97c87a5
a.zhavarankau <a.zhavarankau@gmail.com>
2022-11-10 18:24:36 +0100

    Make first 'wave'

--- Ziggurat/main.py
+++ Ziggurat/main.py
@@ -1,1 +1,18 @@
+import pprint
+n = 4
 
+m = []
+# Prepare 'wave' for the rows
+for i in range(2*n-1):
+    m.append([])
+    for j in range(2*n-1):
+        row = (n-1)-abs(n-1-i)
+        col = 1
+        m[i].append(row)
+
+
+# pprint.pprint(m)
+for row in m:
+    for element in row:
+        print(element, end=" ")
+    print()
